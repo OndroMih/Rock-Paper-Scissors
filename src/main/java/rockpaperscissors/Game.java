3fb9246e272507d5b25b064696c3e1d773049a3c
Ondrej Mihályi <ondrej.mihalyi@gmail.com>
2022-04-27 15:18:04 +0200

    Game restarts Signed-off-by:Ondrej Mihályi <ondrej.mihalyi@gmail.com>

--- src/main/java/rockpaperscissors/Game.java
+++ src/main/java/rockpaperscissors/Game.java
@@ -35,7 +35,7 @@ public class Game {
     }
 
     public void restart() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        rounds.clear();
     }
     
 }
